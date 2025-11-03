e23be660afaa7a3726f745441363155b963a3406
demna-miqava <demnamikava11@gmail.com>
2025-11-03 16:38:59 +0400

    provide pinia

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { VueQueryPlugin } from "@tanstack/vue-query";
+import { createPinia } from "pinia";
 
 import "./main.css";
 
-createApp(App).use(router).use(VueQueryPlugin).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(router).use(VueQueryPlugin).use(pinia).mount("#app");
