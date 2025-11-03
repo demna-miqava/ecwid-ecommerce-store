0cbcfbd175642a80c7c262e3671fd2761c16962b
demna-miqava <demnamikava11@gmail.com>
2025-11-03 16:45:50 +0400

    display total cart items in header

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCart } from "@/features/cart/composables/useCart";
+
+const { totalItems } = useCart();
+</script>
 
 <template>
   <header class="bg-white shadow-sm border-b border-gray-200">
@@ -8,10 +12,11 @@
           LightSpeed Assignment
         </router-link>
         <router-link to="/cart" class="relative">
+          <i class="pi pi-shopping-bag text-[24px] text-gray-500"></i>
           <span
             class="absolute -top-2 -right-2 bg-black text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center"
           >
-            0
+            {{ totalItems }}
           </span>
         </router-link>
       </div>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import { createPinia } from "pinia";
 
+import "primeicons/primeicons.css";
 import "./main.css";
 
 const pinia = createPinia();
