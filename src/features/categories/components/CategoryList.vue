33bb3d80e650e8f1b5cb10911dfefb81c191cb31
demna-miqava <demnamikava11@gmail.com>
2025-11-03 22:54:19 +0400

    use semantically correct tag

--- src/features/categories/components/CategoryList.vue
+++ src/features/categories/components/CategoryList.vue
@@ -31,7 +31,7 @@ const nestedCategories = computed(() => {
     </template>
 
     <template #default="{ data }">
-      <div
+      <section
         class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
       >
         <CategoryItem
@@ -39,7 +39,7 @@ const nestedCategories = computed(() => {
           :key="category.id"
           :category="category"
         />
-      </div>
+      </section>
     </template>
   </FetchWrapper>
 </template>
