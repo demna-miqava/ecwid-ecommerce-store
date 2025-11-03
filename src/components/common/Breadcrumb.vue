1fdd46450bf26b20a09805e68ce1db575727b01f
demna-miqava <demnamikava11@gmail.com>
2025-11-03 19:41:15 +0400

    add breadcrumb in categories

---
+++ src/components/common/Breadcrumb.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+export interface BreadcrumbItem {
+  label: string;
+  to?: string;
+}
+
+interface Props {
+  items: BreadcrumbItem[];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <nav aria-label="Breadcrumb" class="mb-6">
+    <ol class="flex items-center space-x-1 text-sm">
+      <li v-for="(item, index) in items" :key="index" class="flex items-center">
+        <router-link
+          v-if="item.to"
+          :to="item.to"
+          class="text-gray-600 hover:text-gray-900 transition-colors"
+        >
+          {{ item.label }}
+        </router-link>
+        <span v-else class="text-gray-900 font-medium">
+          {{ item.label }}
+        </span>
+        <i
+          v-if="index < items.length - 1"
+          class="pi pi-chevron-right text-gray-400 text-xs mx-2"
+        ></i>
+      </li>
+    </ol>
+  </nav>
+</template>
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -4,6 +4,9 @@ import { useRoute } from "vue-router";
 import { useGetCategories } from "@/features/categories/composables/useGetCategories";
 import SubcategoriesSection from "@/features/categories/components/SubcategoriesSection.vue";
 import CategoryProductsSection from "@/features/categories/components/CategoryProductsSection.vue";
+import Breadcrumb, {
+  type BreadcrumbItem,
+} from "@/components/common/Breadcrumb.vue";
 
 const route = useRoute();
 
@@ -22,10 +25,47 @@ const subcategories = computed(
       (cat) => cat.parentId === currentCategoryId.value
     ) ?? []
 );
+
+const currentCategory = computed(() =>
+  categoriesData.value?.items.find((cat) => cat.id === currentCategoryId.value)
+);
+
+// Find parent category if viewing a subcategory
+const parentCategory = computed(() => {
+  if (!currentCategory.value?.parentId) return null;
+  return categoriesData.value?.items.find(
+    (cat) => cat.id === currentCategory.value!.parentId
+  );
+});
+
+const breadcrumbItems = computed<BreadcrumbItem[]>(() => {
+  const items: BreadcrumbItem[] = [{ label: "Products", to: "/" }];
+
+  if (parentCategory.value) {
+    // Viewing a subcategory - show parent category
+    items.push({
+      label: parentCategory.value.name,
+      to: `/category/${parentCategory.value.id}`,
+    });
+  }
+
+  if (currentCategory.value) {
+    // Current category (no link as it's the current page)
+    items.push({
+      label: currentCategory.value.name,
+    });
+  }
+
+  return items;
+});
 </script>
 
 <template>
-  <SubcategoriesSection :subcategories="subcategories" />
+  <div class="container mx-auto px-4 py-8">
+    <Breadcrumb :items="breadcrumbItems" />
 
-  <CategoryProductsSection :category-id="currentCategoryId" />
+    <SubcategoriesSection :subcategories="subcategories" />
+
+    <CategoryProductsSection :category-id="currentCategoryId" />
+  </div>
 </template>
